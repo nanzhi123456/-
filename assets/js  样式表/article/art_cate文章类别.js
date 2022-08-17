41fcae287234eb2e2e1f57b0f4c69563b922997f
nanzhi <1713907876@qq.com>
2022-08-17 17:38:33 +0800

    最新的

--- assets/js  样式表/article/art_cate文章类别.js
+++ assets/js  样式表/article/art_cate文章类别.js
@@ -11,12 +11,38 @@ $(function () {
             }
         })
     }
-    $('#btnAddCate').on('click',function(){
-        layer.open({
-            tyep:1,
-            title: '添加文章分类'
-            ,content: '配置各种参数，试试效果',
-            area:['500px','250px']
-          });   
+    var indexAdd = null
+    $('#btnAddCate').on('click', function () {
+        indexAdd = layer.open({
+            type: 1,
+            area: ['500px', '330px'],
+            title: '添加文章分类',
+            content: $('#dialog-add').html()
+        })
+    })
+    $('body').on('submit', '#form-add', function (e) {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/addcates',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) return layer.msg('新增分类失败')
+                initArtCateList()
+                layer.msg('新增分类成功！')
+                // 根据索引，关闭对应的弹出层
+                layer.close(indexAdd)
+            }
+    
+        })
+    })
+    var indexEdit= null
+    $('tbody').on('click', '.btn-edit', function (e) {
+        indexEdit= layer.open({
+            type: 1,
+            area: ['500px', '330px'],
+            title: '修该文章分类',
+            content: $('#dialog-edit').html()
+        })
     })
 })
