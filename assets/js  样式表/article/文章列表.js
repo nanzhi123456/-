8958603f3ec746678ce24956ee4eab5c661cea3a
nanzhi <1713907876@qq.com>
2022-08-20 11:47:24 +0800

    最新的

--- assets/js  样式表/article/文章列表.js
+++ assets/js  样式表/article/文章列表.js
@@ -39,7 +39,6 @@ $(function () {
             data: q,
             success: function (res) {
                 if (res.status !== 0) return layer.msg('获取文章列表失败')
-                layer.msg('获取文章列表成功')
                 // 使用模板引擎渲染页面的数据
                 var hetmlstr = template('tpl-table', res)
                 $('tbody').html(hetmlstr)
@@ -66,7 +65,6 @@ $(function () {
     $('#form-search').on('submit', function (e) {
         e.preventDefault();
         var cate_id = $('[name=cate_id').val();
-        console.log(cate_id);
         var state = $('[name=state').val();
         q.cate_id = cate_id;
         q.state = state;
@@ -76,6 +74,8 @@ $(function () {
     function renderPage(total) {
         // console.log(total);
         laypage.render({
+            layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+            limits: [2, 3, 5, 10],
             // 分页容器的id
             elem: 'pageBox' //注意，这里的 test1 是 ID，不用加 # 号
             // 总数据条数
@@ -83,10 +83,47 @@ $(function () {
             // 每页显示几天的数据
             limit: q.pagesize,
             // 默认显示的分页
-            curr: q.pagenum
+            curr: q.pagenum,
+            // 分页切换触发的回调
+            jump: function (obj, first) {
+                // console.log(obj.curr);
+                // 把最新的页码值赋值到q的查询对象中
+                q.pagenum = obj.curr
+                // 这是根据最新的q获取对应的列表并渲染表格
+                q.pagesize = obj.limit
+                if (!first) {
+                    initTable()
+                }
+            }
         });
     }
+    $('tbody').on('click', '.btn-delete', function () {
+        var id = $(this).attr('data-id')
+        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function (index) {
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/deletecate/' + id,
+                success: function (res) {
+                    console.log(res);
+                    if (res.status !== 0) {
+                        return layer.msg('删除分类失败！')
+                    }
+                    layer.msg('删除分类成功！')
+                    layer.close(index)
+                    initTable()
+                }
+            })
+        })
+    })
+    // 文章编辑  晚自习再写
     $('tbody').on('click', '.btn-bianji', function () {
-        
+        //发布文章
+        //编辑点击事件
+        //给跳转链接绑定文章id
+        //发起GET请求获取文章详情
+        //数据回显  文章类别的ajax是请求回来的  是异步任务 比数据回显慢
+        //富文本编辑器是一打开就显示, 先给他赋值数据回显的数据 再初始化渲染
+        //图片的GET请求要拼接图片的域名才可以显示
+
     })
 })
